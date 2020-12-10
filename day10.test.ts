1d16de1994e54f1344354944bac55dccd149b46d
Robin Pokorny <robin.pokorny@klarna.com>
2020-12-10 22:23:04 +0100

    solve 10b

--- day10.test.ts
+++ day10.test.ts
@@ -11,6 +11,11 @@ const last = <T>(xs: T[]): T => xs[xs.length - 1];
 const empty = (): Diffs => ({ 1: 0, 3: 0 });
 const toDiff = (xs: number[]): (1 | 3)[] =>
   xs.map((x, i) => (x - (xs[i - 1] || 0)) as 1 | 3);
+const product = (a, b) => a * b;
+
+// Ways of passing x ones in a row
+const COMBINATIONS = [1, 1, 2, 4, 7];
+const toCombinations = (x: number) => COMBINATIONS[x];
 
 const countOccurences = (xs: (1 | 3)[]) =>
   xs.reduce((diffs, x) => {
@@ -26,6 +31,15 @@ const joltageDiff = (adapters: number[]): Diffs => {
 
   return countOccurences(toDiff(withDevice));
 };
+
+const countCombinations = (adapters: number[]): number =>
+  toDiff(adapters)
+    .join("")
+    .split("3") // get rows of ones in between 3s
+    .filter(Boolean)
+    .map((group) => group.length)
+    .map(toCombinations)
+    .reduce(product, 1);
 
 /* === TESTS === */
 
@@ -43,9 +57,17 @@ test("Day <day>a - prod", () => {
   expect(diffs[1] * diffs[3]).toBe(1836);
 });
 
-test.skip("Day <day>b - test", () => {});
+test("Day <day>b - test", () => {
+  const result = countCombinations(testInput);
 
-test.skip("Day <day>b - prod", () => {});
+  expect(result).toBe(19208);
+});
+
+test("Day <day>b - prod", () => {
+  const result = countCombinations(prodInput);
+
+  expect(result).toBe(43406276662336);
+});
 
 /* === INPUTS === */
 
