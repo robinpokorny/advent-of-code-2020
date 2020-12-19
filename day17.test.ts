1787c8cf88542c1dfda8fea91555c61da431a5e3
Robin Pokorny <robin.pokorny@klarna.com>
2020-12-19 21:09:58 +0100

    solve 17b

--- day17.test.ts
+++ day17.test.ts
@@ -1,46 +1,61 @@
 /* === TYPES === */
 type World = Set<string>;
 
-type Point = [number, number, number];
+type Point = number[];
+
+type GetNeighbours = (point: string | Point) => Point[];
 /* === PREPARE INPUT === */
 
 export const prepareInput = ([input]: TemplateStringsArray) =>
   input.split("\n").map((line) => line.split(""));
 
 /* === UTILS === */
-const getActive = (input: string[][]) => {
+const ROUND = [-1, 0, 1];
+
+const cartesian = <T>(...sets: T[][]) =>
+  sets.reduce((acc, set) => acc.flatMap((x) => set.map((y) => [...x, y])), [
+    [],
+  ] as T[][]);
+
+const getActive = (dim: number, input: string[][]) => {
   const world: World = new Set();
 
   input.forEach((column, x) =>
     column.forEach((point, y) => {
-      if (point === "#") world.add(`${x},${y},0`);
+      if (point === "#") world.add(`${x},${y}${",0".repeat(dim - 2)}`);
     })
   );
 
   return world;
 };
 
-// prettier-ignore
-const neighbours = [
-  [-1, -1, -1], [-1, 0, -1], [-1, 1, -1],
-  [ 0, -1, -1], [ 0, 0, -1], [ 0, 1, -1],
-  [ 1, -1, -1], [ 1, 0, -1], [ 1, 1, -1],
-  [-1, -1,  0], [-1, 0,  0], [-1, 1,  0],
-  [ 0, -1,  0], /*0, 0,  0*/ [ 0, 1,  0],
-  [ 1, -1,  0], [ 1, 0,  0], [ 1, 1,  0],
-  [-1, -1,  1], [-1, 0,  1], [-1, 1,  1],
-  [ 0, -1,  1], [ 0, 0,  1], [ 0, 1,  1],
-  [ 1, -1,  1], [ 1, 0,  1], [ 1, 1,  1],
-];
+const neighbours3 = cartesian(ROUND, ROUND, ROUND).filter(
+  (point) => !point.every((x) => x === 0)
+);
 
-const getNeighbours = (point: string | Point): Point[] => {
+const getNeighbours3: GetNeighbours = (point) => {
   const [x, y, z] =
     typeof point === "string" ? point.split(",").map(Number) : point;
-  return neighbours.map(([dx, dy, dz]) => [x + dx, y + dy, z + dz]);
+  return neighbours3.map(([dx, dy, dz]) => [x + dx, y + dy, z + dz]);
+};
+
+const neighbours4 = cartesian(ROUND, ROUND, ROUND, ROUND).filter(
+  (point) => !point.every((x) => x === 0)
+);
+
+const getNeighbours4: GetNeighbours = (point) => {
+  const [x, y, z, w] =
+    typeof point === "string" ? point.split(",").map(Number) : point;
+  return neighbours4.map(([dx, dy, dz, dw]) => [
+    x + dx,
+    y + dy,
+    z + dz,
+    w + dw,
+  ]);
 };
 
 /* === IMPLEMENTATION === */
-const nextWorld = (world: World): World =>
+const nextWorld = (getNeighbours: GetNeighbours) => (world: World): World =>
   new Set(
     [...world]
       .flatMap(getNeighbours)
@@ -64,18 +79,36 @@ const nextWorld = (world: World): World =>
 /* === TESTS === */
 
 test("Day 17a - test", () => {
-  const result = Array(6).fill(0).reduce(nextWorld, getActive(testInput));
+  const result = Array(6)
+    .fill(0)
+    .reduce(nextWorld(getNeighbours3), getActive(3, testInput));
+
   expect(result.size).toBe(112);
 });
 
 test("Day 17a - prod", () => {
-  const result = Array(6).fill(0).reduce(nextWorld, getActive(prodInput));
+  const result = Array(6)
+    .fill(0)
+    .reduce(nextWorld(getNeighbours3), getActive(3, prodInput));
+
   expect(result.size).toBe(346);
 });
 
-test.skip("Day 17b - test", () => {});
+test("Day 17b - test", () => {
+  const result = Array(6)
+    .fill(0)
+    .reduce(nextWorld(getNeighbours4), getActive(4, testInput));
 
-test.skip("Day 17b - prod", () => {});
+  expect(result.size).toBe(848);
+});
+
+test("Day 17b - prod", () => {
+  const result = Array(6)
+    .fill(0)
+    .reduce(nextWorld(getNeighbours4), getActive(4, prodInput));
+
+  expect(result.size).toBe(1632);
+});
 
 /* === INPUTS === */
 
